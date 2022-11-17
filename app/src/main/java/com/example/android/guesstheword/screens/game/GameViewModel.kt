82f8ce76e2d4b89d917b4d9e296e50dac1b146f7
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-11-17 22:15:12 +0200

    add events and live data
    to allow encapsulation

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,25 +1,34 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 
     // The current word
-    var word = MutableLiveData<String>()
+    var _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
     // The current score
-    var score = MutableLiveData<Int>()
+    private var _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
+    private var _eventGamefinished = MutableLiveData<Boolean>()
+    val eventGameFinished: LiveData<Boolean>
+        get () = _eventGamefinished
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
+        _eventGamefinished.value = false
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
-        score.value = 0
-        word.value = ""
+        _score.value = 0
+        _word.value = ""
     }
 
     override fun onCleared() {
@@ -63,21 +72,25 @@ class GameViewModel: ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
+            _eventGamefinished.value = true
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
+    fun onGameFinishComplete() {
+        _eventGamefinished.value = false
+    }
 }

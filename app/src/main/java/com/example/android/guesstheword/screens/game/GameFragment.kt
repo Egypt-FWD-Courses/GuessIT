c0a71933f649d16e814766975046226e87f9ae88
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-11-17 20:45:10 +0200

    Create a view model for game fragment

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -33,7 +34,7 @@ class GameFragment : Fragment() {
 
     // The current word
     private var word = ""
-
+    private lateinit var viewModel:GameViewModel
     // The current score
     private var score = 0
 
@@ -52,7 +53,7 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
-
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
         resetList()
         nextWord()
 
